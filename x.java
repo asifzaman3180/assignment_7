520f488df642ccac58a260c25ed48d566ed204f6
souravsikderjoy <s2410676135@ru.ac.bd>
2025-11-10 12:04:31 +0600

    cleaned variables and split logic into seperate classes

--- x.java
+++ x.java
@@ -1,97 +1,119 @@
 import java.util.*;
 
-public class x{
-    public static void main(String args[]){
-        Scanner s=new Scanner(System.in);
-        System.out.println("Enter no. of students: ");
-        int n=s.nextInt();
-        String nm[]=new String[n];
-        int id[]=new int[n];
-        int m1[]=new int[n];
-        int m2[]=new int[n];
-        int m3[]=new int[n];
-        double avg[]=new double[n];
-        double top=0;
-        int topIndex=0;
-        for(int i=0;i<n;i++){
-        System.out.println("Enter name:");
-        nm[i]=s.next();
-        System.out.println("Enter id:");
-        id[i]=s.nextInt();
-        System.out.println("Enter marks of 3 subjects:");
-        m1[i]=s.nextInt();
-        m2[i]=s.nextInt();
-        m3[i]=s.nextInt();
-        avg[i]=(m1[i]+m2[i]+m3[i])/3.0;
-        if(avg[i]>top){
-        top=avg[i];
-        topIndex=i;
+class Student {
+    String name;
+    int id;
+    int mark1;
+    int mark2;
+    int mark3;
+    double average;
+
+    public Student(String name, int id, int mark1, int mark2, int mark3) {
+        this.name = name;
+        this.id = id;
+        this.mark1 = mark1;
+        this.mark2 = mark2;
+        this.mark3 = mark3;
+        this.average = (mark1 + mark2 + mark3) / 3.0;
+    }
+
+    public String calculateGrade() {
+        if (average >= 80) return "A+";
+        else if (average >= 70) return "A";
+        else if (average >= 60) return "B";
+        else if (average >= 50) return "C";
+        else return "F";
+    }
+}
+
+class StudentService {
+    public void displayAll(List<Student> students) {
+        for (Student s : students) {
+            System.out.println("Name: " + s.name + " Id: " + s.id + " Avg: " + s.average);
         }
     }
+
+    public Student findTopper(List<Student> students) {
+        Student topper = students.get(0);
+        for (Student s : students) {
+            if (s.average > topper.average) topper = s;
+        }
+        return topper;
+    }
+
+    public void sortByAverage(List<Student> students) {
+        students.sort((a, b) -> Double.compare(b.average, a.average));
+    }
+
+    public Student searchById(List<Student> students, int id) {
+        for (Student s : students) {
+            if (s.id == id) return s;
+        }
+        return null;
+    }
+
+    public void displayGrades(List<Student> students) {
+        for (Student s : students) {
+            System.out.println(s.name + " Grade: " + s.calculateGrade());
+        }
+    }
+}
+
+public class x {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        StudentService service = new StudentService();
+        List<Student> students = new ArrayList<>();
+
+        System.out.print("Enter number of students: ");
+        int n = scanner.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("Enter name:");
+            String name = scanner.next();
+            System.out.println("Enter id:");
+            int id = scanner.nextInt();
+            System.out.println("Enter marks of 3 subjects:");
+            int m1 = scanner.nextInt();
+            int m2 = scanner.nextInt();
+            int m3 = scanner.nextInt();
+
+            students.add(new Student(name, id, m1, m2, m3));
+        }
+
         System.out.println("All Students:");
-        for(int i=0;i<n;i++){
-        System.out.println("Name:"+nm[i]+" Id:"+id[i]+" Avg:"+avg[i]);
-        }
-        System.out.println("Topper: "+nm[topIndex]+" Avg:"+avg[topIndex]);
+        service.displayAll(students);
+
+        Student topper = service.findTopper(students);
+        System.out.println("Topper: " + topper.name + " Avg: " + topper.average);
+
         System.out.println("Sort by Average? y/n");
-        String c=s.next();
-        if(c.equals("y")){
-        for(int i=0;i<n-1;i++){
-        for(int j=i+1;j<n;j++){
-        if(avg[i]<avg[j]){
-        String tnm=nm[i];
-        nm[i]=nm[j];
-        nm[j]=tnm;
-        int tid=id[i];
-        id[i]=id[j];
-        id[j]=tid;
-        int tm1=m1[i];
-        m1[i]=m1[j];
-        m1[j]=tm1;
-        int tm2=m2[i];
-        m2[i]=m2[j];
-        m2[j]=tm2;
-        int tm3=m3[i];
-        m3[i]=m3[j];
-        m3[j]=tm3;
-        double tav=avg[i];
-        avg[i]=avg[j];
-        avg[j]=tav;
+        String choice = scanner.next();
+        if (choice.equals("y")) {
+            service.sortByAverage(students);
+            System.out.println("Sorted List:");
+            service.displayAll(students);
         }
-        }
-    }
-        System.out.println("Sorted List:");
-        for(int i=0;i<n;i++){
-        System.out.println(nm[i]+" "+avg[i]);
-    }
-    }
+
         System.out.println("Search student by id?");
-        String d=s.next();
-        if(d.equals("y")){
-        System.out.println("Enter id:");
-        int sid=s.nextInt();
-        boolean f=false;
-        for(int i=0;i<n;i++){
-        if(id[i]==sid){
-        System.out.println("Found:"+nm[i]+" Avg:"+avg[i]);
-        f=true;
+        String searchChoice = scanner.next();
+        if (searchChoice.equals("y")) {
+            System.out.println("Enter id:");
+            int searchId = scanner.nextInt();
+            Student found = service.searchById(students, searchId);
+            if (found != null)
+                System.out.println("Found: " + found.name + " Avg: " + found.average);
+            else
+                System.out.println("Not found");
         }
-    }
-        if(!f) System.out.println("Not found");
-    }
+
         System.out.println("Calculate grade?");
-        String g=s.next();
-        if(g.equals("y")){
-        for(int i=0;i<n;i++){
-        String grade="";
-        if(avg[i]>=80) grade="A+";
-        else if(avg[i]>=70) grade="A";
-        else if(avg[i]>=60) grade="B";
-        else if(avg[i]>=50) grade="C";
-        else grade="F";
-        System.out.println(nm[i]+" Grade:"+grade);
+        String gradeChoice = scanner.next();
+        if (gradeChoice.equals("y")) {
+            service.displayGrades(students);
         }
-    }
+
         System.out.println("Bye!");
+        scanner.close();
     }
 }
