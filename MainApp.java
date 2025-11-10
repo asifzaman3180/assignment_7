22dd737941490635c58fd855ed361c40d4e42d71
simanta <s2410476123@ru.ac.bd>
2025-11-10 12:02:12 +0600

    documentation

--- MainApp.java
+++ MainApp.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-
+// Student class to store student info and calculate average 
 public class MainApp {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
