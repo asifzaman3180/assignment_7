9bf5f5f11b4a467347189541361de2b8cd47046d
Ahsan Hasby <s2410876132@ru.ac.bd>
2025-11-10 12:07:15 +0600

    Modularized methods

--- x.java
+++ x.java
@@ -1,28 +1,28 @@
 import java.util.*;
 
 public class x{
-    public static void main(String args[]) {
+
+    static void Results(int Total) {
         Scanner s = new Scanner(System.in);
-        System.out.println("Enter no. of students: ");
-        int Total=s.nextInt();
         String name[]=new String[Total];
         int Student_ID[]=new int[Total];
-        int Subject_1_mark[]=new int[Total];
-        int Subject_2_mark[]=new int[Total];
-        int Subject_3_mark[]=new int[Total];
+        int Subject1_mark[]=new int[Total];
+        int Subject2_mark[]=new int[Total];
+        int Subject3_mark[]=new int[Total];
         double Average_marks[]=new double[Total];
         double top=0;
         int topIndex=0;
+
         for(int i=0;i<Total;i++) {
             System.out.println("Enter name:");
             name[i]=s.next();
             System.out.println("Enter Student_ID:");
             Student_ID[i]=s.nextInt();
             System.out.println("Enter marks of 3 subjects:");
-            Subject_1_mark[i]=s.nextInt();
-            Subject_2_mark[i]=s.nextInt();
-            Subject_3_mark[i]=s.nextInt();
-            Average_marks[i]=(Subject_1_mark[i]+Subject_2_mark[i]+Subject_3_mark[i])/3.0;
+            Subject1_mark[i]=s.nextInt();
+            Subject2_mark[i]=s.nextInt();
+            Subject3_mark[i]=s.nextInt();
+            Average_marks[i]=(Subject1_mark[i]+Subject2_mark[i]+Subject3_mark[i])/3.0;
             if(Average_marks[i]>top) {
                 top=Average_marks[i];
                 topIndex=i;
@@ -36,9 +36,10 @@ public class x{
         }
 
         System.out.println("Topper: "+name[topIndex]+" Average_marks:"+Average_marks[topIndex]);
-        System.out.println("Sort by Average? y/n");
-        String c=s.next();
+        
+    }
 
+    static void Sort_Students(String name[], double Average_marks[], int Subject1_mark[], int Subject2_mark[], int Subject3_mark[], int Student_ID[], int Total, String c) {
         if(c.equals("y")) {
             for(int i=0;i<Total-1;i++) {
                 for(int j=i+1;j<Total;j++) {
@@ -49,18 +50,18 @@ public class x{
                     int Temp_ID=Student_ID[i];
                     Student_ID[i]=Student_ID[j];
                     Student_ID[j]=Temp_ID;
-                    int Temp1_mark=Subject_1_mark[i];
-                    Subject_1_mark[i]=Subject_1_mark[j];
-                    Subject_1_mark[j]=Temp1_mark;
-                    int Temp2_mark=Subject_2_mark[i];
-                    Subject_2_mark[i]=Subject_2_mark[j];
-                    Subject_2_mark[j]=Temp2_mark;
-                    int Temp3_mark=Subject_3_mark[i];
-                    Subject_3_mark[i]=Subject_3_mark[j];
-                    Subject_3_mark[j]=Temp3_mark;
-                    double tav=Average_marks[i];
+                    int Temp1_mark=Subject1_mark[i];
+                    Subject1_mark[i]=Subject1_mark[j];
+                    Subject1_mark[j]=Temp1_mark;
+                    int Temp2_mark=Subject2_mark[i];
+                    Subject2_mark[i]=Subject2_mark[j];
+                    Subject2_mark[j]=Temp2_mark;
+                    int Temp3_mark=Subject3_mark[i];
+                    Subject3_mark[i]=Subject3_mark[j];
+                    Subject3_mark[j]=Temp3_mark;
+                    double Temp_average=Average_marks[i];
                     Average_marks[i]=Average_marks[j];
-                    Average_marks[j]=tav;
+                    Average_marks[j]=Temp_average;
                     }
                 }
             }
@@ -69,9 +70,13 @@ public class x{
                 System.out.println(name[i]+" "+Average_marks[i]);
             }
         }
-        System.out.println("Search student by Student_ID?");
-        String d=s.next();
+    }
+
+    static void Search_ID(String name[], double Average_marks[], int Student_ID[], String d, int Total) {
+
         if(d.equals("y")) {
+            Scanner s = new Scanner(System.in);
+
             System.out.println("Enter Student_ID:");
             int Temp_ID=s.nextInt();
             boolean f=false;
@@ -83,10 +88,9 @@ public class x{
             }
             if(!f) System.out.println("Not found");
         }
+    }
 
-        System.out.println("Calculate grade?");
-        String g=s.next();
-
+    static void Calculate_grade(double Average_marks[], String name[], String g, int Total) {
         if(g.equals("y")) {
             for(int i=0;i<Total;i++) {
                 String grade="";
@@ -103,6 +107,35 @@ public class x{
                 System.out.println(name[i]+" Grade:"+grade);
             }
         }
+    }
+
+    public static void main(String args[]) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter no. of students: ");
+        int Total=s.nextInt();
+        String name[]=new String[Total];
+        int Student_ID[]=new int[Total];
+        int Subject1_mark[]=new int[Total];
+        int Subject2_mark[]=new int[Total];
+        int Subject3_mark[]=new int[Total];
+        double Average_marks[]=new double[Total];
+
+        Results(Total);
+
+        System.out.println("Sort by Average? y/n");
+        String c=s.next();
+
+        Sort_Students(name, Average_marks, Subject1_mark, Subject2_mark, Subject3_mark, Student_ID, Total, c);
+        
+        System.out.println("Search student by Student_ID?");
+        String d=s.next();
+        
+        Search_ID(name, Average_marks, Student_ID, d, Total);
+
+        System.out.println("Calculate grade?");
+        String g=s.next();
+
+        Calculate_grade(Average_marks, name, g, Total);
         
         System.out.println("Bye!");
     }
